0ae846044740d2e753a9206f6da1b03e1bfa61f6
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-01-21 17:24:35 +0900

    page redirect modified

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import Router, { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  
+  useEffect(() => {
+    const id = localStorage.getItem("id_token");
+    const access = localStorage.getItem("access_token");
+    if(id != null && access != null){
+      //アクティビティページに飛ばす
+      Router.push("/activities");
+    }
+  },[])
+
+
+
   return <Component {...pageProps} />
+
 }
